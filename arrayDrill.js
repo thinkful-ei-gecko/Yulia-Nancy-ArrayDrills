67a97672271048ece0daf27f83d48a7785f12928
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-08-13 09:07:36 -0700

    min function

--- arrayDrill.js
+++ arrayDrill.js
@@ -1,25 +1,34 @@
-
+'use strict';
 
 function max(numbers){
-    let i = 0;
-    let start = numbers[0];
+  let i = 0;
+  let start = numbers[0];
 
-    while (i < numbers.length){
-        if(numbers[i] > start) {
-            start = numbers[i]
-        }
-        i++;
+  while (i < numbers.length){
+    if(numbers[i] > start) {
+      start = numbers[i];
     }
-    return start;
+    i++;
+  }
+  return start;
 }
 
 
 
-console.log(max([2, 3, 90, 10]))
+//console.log(max([2, 3, 90, 10]));
 
 
 
 function min(numbers){
-
-
+  let i = 0;
+  let currentMin = numbers[0];
+  while (i < numbers.length) {
+    if (numbers[i] < currentMin) {
+      currentMin = numbers[i];
+    }
+    i++;
+  }
+  return currentMin;
 }
+
+console.log(min([2, 3, 90, 1, 10]));
